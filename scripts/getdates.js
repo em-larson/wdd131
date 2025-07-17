112a02b0fd0c418d5dba4123520755c8b8bd5b0a
em-larson <elarson1@byupathway.edu>
2025-07-17 12:39:19 -0600

    Finshed Week 2 project

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,5 +5,3 @@ document.getElementById("year").innerHTML = `&copy ${year} | Emily Larson | Utah
 const mm = new Date().getMonth() + 1;
 const dd = new Date().getDate();
 document.getElementById("date").innerHTML = `Date Modified: ${mm}/${dd}/${year}`;
-
-document.querySelector
--- scripts/temples.js
+++ scripts/temples.js
@@ -2,5 +2,13 @@ const year = new Date().getFullYear();
 const month = new Date().getMonth() + 1;
 const day = new Date().getDate();
 
-document.getElementById("copyright").textContent = `&copy ${year} | Emily Larson | Utah, USA`;
+document.getElementById("copyright").innerHTML = `&copy ${year} | Emily Larson | Utah, USA`;
 document.getElementById('modified').textContent = `Date Modified: ${month}/${day}/${year}`;
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector('nav');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});

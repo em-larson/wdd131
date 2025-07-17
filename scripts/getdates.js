a0eec75237a519a8db5f2ecf496157e3e2368237
em-larson <elarson1@byupathway.edu>
2025-07-17 10:46:46 -0600

    fixed bug

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,5 @@
-const year = new Date().getFullYear()
-document.getElementById("year").innerHTML = `&copy ${year}`;
+const year = new Date().getFullYear();
+document.getElementById("year").innerHTML = `&copy ${year} | Emily Larson | Utah, USA`;
 
 // const date = new Date().getDate()
 const mm = new Date().getMonth() + 1;
---
+++ scripts/temples.js
@@ -0,0 +1,6 @@
+const year = new Date().getFullYear();
+const month = new Date().getMonth() + 1;
+const day = new Date().getDate();
+
+document.getElementById("copyright").textContent = `&copy ${year} | Emily Larson | Utah, USA`;
+document.getElementById('modified').textContent = `Date Modified: ${month}/${day}/${year}`;

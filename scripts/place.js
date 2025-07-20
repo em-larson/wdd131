b436c63171eda369c782f34648b6d4b65b5d7809
em-larson <elarson1@byupathway.edu>
2025-07-19 18:32:56 -0600

    Finished W03 Assignment

---
+++ scripts/place.js
@@ -0,0 +1,20 @@
+const year = new Date().getFullYear();
+const month = new Date().getMonth() + 1;
+const day = new Date().getDate();
+
+document.getElementById("copyright").innerHTML = `&copy ${year} | Emily Larson | Utah, USA`;
+document.getElementById('modified').textContent = `Date Modified: ${month}/${day}/${year}`;
+
+
+function windchill(temp, windspeed) {
+    if (temp < 50 && windspeed > 3) {
+        let wc;
+        wc = 35.74 + (0.6215 * temp) - (35.75 * (windspeed ** 0.16)) + (0.4275 * temp * (windspeed ** 0.16));
+        return wc;
+    }
+    else {
+        return "N/A";
+    }
+}
+
+document.getElementById("windchill").innerHTML = ``;

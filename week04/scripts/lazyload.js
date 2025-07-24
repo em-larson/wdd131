8dd683362ea66ce3a1e295332795f7dc5fe57ff3
em-larson <elarson1@byupathway.edu>
2025-07-24 15:22:18 -0600

    Finished Week 04 project

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,7 @@
+const year = new Date().getFullYear();
+const month = new Date().getMonth() + 1;
+const day = new Date().getDate();
+
+document.getElementById("copyright").innerHTML = `&copy ${year} | Emily Larson | Utah, USA`;
+document.getElementById('modified').textContent = `Date Modified: ${month}/${day}/${year}`;
+

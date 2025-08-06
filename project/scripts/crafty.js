4be7814cb97fa2a2f1e9953b88a13c344cba19fa
em-larson <elarson1@byupathway.edu>
2025-08-05 18:01:44 -0600

    fixed the phone version formatting. Just needs pictures now

--- project/scripts/crafty.js
+++ project/scripts/crafty.js
@@ -1,3 +1,5 @@
+//footer code//
+
 const year = new Date().getFullYear();
 const month = new Date().getMonth() + 1;
 const day = new Date().getDate();
@@ -5,20 +7,45 @@ const day = new Date().getDate();
 document.getElementById("copyright").innerHTML = `&copy ${year} | Emily Larson | Utah, USA`;
 document.getElementById('modified').textContent = `Date Modified: ${month}/${day}/${year}`;
 
+//responsive menu code//
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector('#mainNav');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+//submit button on join page//
 
 const submitQuestion = document.getElementById('submit');
-submitQuestion.addEventListener("click", () => {
 
-    const formBox = document.getElementById('questionaire');
-    formBox.innerHTML = "";
+if (submitQuestion) {
+    submitQuestion.addEventListener("click", (function (event) {
 
-    let thanks = document.createElement("h3");
-    let paragraph = document.createElement("p");
+        event.preventDefault()
 
-    thanks.textContent = "Thank You!"
-    paragraph.textContent = "We have recieved your question. You can expect to hear back from us within a week. We look forward to talking with you!"
+        const formBox = document.getElementById('questionaire');
 
-    formBox.appendChild(thanks);
-    formBox.appendChild(paragraph);
+        if (formBox.checkValidity()) {
 
-})
+            formBox.innerHTML = "";
+
+            let thanks = document.createElement("h3");
+            let paragraph = document.createElement("p");
+
+            thanks.textContent = "Thank You!"
+            paragraph.textContent = "We have recieved your question. You can expect to hear back from us within a week. We look forward to talking with you!"
+
+            formBox.appendChild(thanks);
+            formBox.appendChild(paragraph);
+        }
+
+        else {
+            formBox.reportValidity();
+        }
+
+    }))
+};
+
